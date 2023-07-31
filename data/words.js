f4498f3263f3c2f52f91b69893f4c6ccf7513d09
quro4ka <quro4ka@gmail.com>
2023-07-31 19:42:04 +0300

    fead: add data words

--- data/words.js
+++ data/words.js
@@ -51,4 +51,134 @@ export const words = [
     ],
     img: 'https://img.freepik.com/free-vector/illustration-weather-forecast_53876-20623.jpg?w=2000&t=st=1690816677~exp=1690817277~hmac=bf1f0766e2bcf3fbb7632d50558b04d43f77430c9e7a9bbe5d6297ebcd811ac9',
   },
+  {
+    id: 5,
+    word_en: 'tv show',
+    word_ru: 'ТВ шоу',
+    transcription: '[a:nt]',
+    sentences: [
+      {
+        en: 'i always watch this tv show',
+        ru: 'я всегда смотрю это телешоу',
+      },
+    ],
+    img: 'https://img.freepik.com/free-vector/tv-night-show-with-guest-female-celebrity-giving-interview-television-presenter-broadcasting-studio_33099-1619.jpg?w=2000&t=st=1690821318~exp=1690821918~hmac=2daa67acc974bdf673715d804b1ad8e6d1a0fc22e2896484ec20914efb224fcb',
+  },
+  {
+    id: 6,
+    word_en: 'jogging',
+    word_ru: 'бег трусцой',
+    transcription: '[a:nt]',
+    sentences: [
+      {
+        en: 'jogging is fun',
+        ru: 'бег это весело',
+      },
+    ],
+    img: 'https://img.freepik.com/free-vector/couple-practicing-trail-run-training_74855-5474.jpg?w=2000&t=st=1690821351~exp=1690821951~hmac=51a048bf767534a7be2f6d1151200cdf0980c9b9252981f9de13c41ead9a6939',
+  },
+  {
+    id: 7,
+    word_en: 'fast food',
+    word_ru: 'быстрое питание',
+    transcription: '[a:nt]',
+    sentences: [
+      {
+        en: 'i crave fast food every once in a while',
+        ru: 'я жажду фаст-фуда время от времени',
+      },
+    ],
+    img: 'https://img.freepik.com/free-vector/collection-handrawn-elements-fast-food_125540-314.jpg?w=2000&t=st=1690821383~exp=1690821983~hmac=ad9a7a4b03ebe3b3c8275552d9d1affd42c9a15aa93114e475bbd2ff52fc05de',
+  },
+  {
+    id: 8,
+    word_en: 'park',
+    word_ru: 'парк',
+    transcription: '[a:nt]',
+    sentences: [
+      {
+        en: 'Let’s go to the park',
+        ru: 'Пойдем в парк',
+      },
+    ],
+    img: 'https://img.freepik.com/free-vector/city-park-with-green-trees-grass-wooden-bench-lanterns-town-buildings-skyline_107791-5378.jpg?w=2000&t=st=1690821421~exp=1690822021~hmac=6ec6ad12a9caef3c5f882cdfad8a9a5f4e2626d0b16432b1922a33be079ffb77',
+  },
+  {
+    id: 9,
+    word_en: 'traffic light',
+    word_ru: 'светофор',
+    transcription: '[a:nt]',
+    sentences: [
+      {
+        en: 'for your safety, check the traffic lights',
+        ru: 'для вашей безопасности проверяйте светофоры',
+      },
+    ],
+    img: 'https://img.freepik.com/free-vector/school-children-teacher-crossing-street-pedestrians-kids-traffic-light-flat-vector-illustration-crosswalk-safety-warning_74855-13231.jpg?w=2000&t=st=1690821463~exp=1690822063~hmac=51361099f9effac278d1e3746a8edfd3e21b9e6b35729749c1df247861e1e890',
+  },
+  {
+    id: 10,
+    word_en: 'sign',
+    word_ru: 'знак',
+    transcription: '[a:nt]',
+    sentences: [
+      {
+        en: 'stop when you see the stop sign',
+        ru: 'остановись, когда увидишь знак остановки',
+      },
+    ],
+    img: 'https://img.freepik.com/free-vector/vector-set-blank-traffic-road-signs_107791-3326.jpg?w=2000&t=st=1690821504~exp=1690822104~hmac=f42f42d47abc48c0d5df23173f85b9402783f34531a5f2c67e1f35ee782d163d',
+  },
+  {
+    id: 11,
+    word_en: 'subway',
+    word_ru: 'метро',
+    transcription: '[a:nt]',
+    sentences: [
+      {
+        en: 'i take the subway to the office',
+        ru: 'я еду на метро в офис',
+      },
+    ],
+    img: 'https://img.freepik.com/free-vector/city-subway-station-cartoon-vector-interior_33099-1832.jpg?w=2000&t=st=1690821558~exp=1690822158~hmac=75de85b6f79d967c2a5365ff3b46224ec2b837b08cdbd3f42f7f6748bd46db18',
+  },
+  {
+    id: 12,
+    word_en: 'train station',
+    word_ru: 'железнодорожная станция',
+    transcription: '[a:nt]',
+    sentences: [
+      {
+        en: 'i can’t find the train station',
+        ru: 'я не могу найти вокзал',
+      },
+    ],
+    img: 'https://img.freepik.com/free-vector/railway-station-with-people-waiting-train-platform_107791-6702.jpg?w=2000&t=st=1690821592~exp=1690822192~hmac=5ef2d80019c408a5ec04feda18e1263bb18b7e16aac0c3d14905a15e1f05d1b8',
+  },
+  {
+    id: 13,
+    word_en: 'five hundred',
+    word_ru: 'пятьсот',
+    transcription: '[a:nt]',
+    sentences: [
+      {
+        en: 'my house is five hundred meters from here',
+        ru: 'мой дом в пятистах метрах отсюда',
+      },
+    ],
+    img: 'https://img.freepik.com/free-vector/500-internal-server-error-concept-illustration_114360-1924.jpg?w=2000&t=st=1690821647~exp=1690822247~hmac=ecd27a77c634db2d101a9b131d3b61b3b2fc5b08ef0481ac965db71571313045',
+  },
+  {
+    id: 14,
+    word_en: 'nine hundred ninety-nine',
+    word_ru: 'девятьсот девяносто девять',
+    transcription: '[a:nt]',
+    sentences: [
+      {
+        en: 'we bought nine hundred ninety-nine chairs for the event',
+        ru: 'мы купили девятьсот девяносто девять стульев для мероприятия',
+      },
+    ],
+    img: 'https://img.freepik.com/free-vector/credit-score-progress-bar_78370-646.jpg?w=1380&t=st=1690821686~exp=1690822286~hmac=e0db0a621c4a492b7edd086ff00d3f4b3d0cdffa98a3696498f343c1f9e7f421',
+  },
 ]
