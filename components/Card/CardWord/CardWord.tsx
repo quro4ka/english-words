dc33a7d6fdb0b93b8a79bd8c065bd07c8b36cba9
quro4ka <quro4ka@gmail.com>
2023-07-31 19:16:42 +0300

    fead: change height

--- components/Card/CardWord/CardWord.tsx
+++ components/Card/CardWord/CardWord.tsx
@@ -39,25 +39,8 @@ export default function CardWord({ word, onNextClick, onPrevClick }: CardWordPro
     setIsView((view) => !view)
   }
 
-  const shimmer = (w: number, h: number) => `
-<svg width="${w}" height="${h}" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
-  <defs>
-    <linearGradient id="g">
-      <stop stop-color="#333" offset="20%" />
-      <stop stop-color="#222" offset="50%" />
-      <stop stop-color="#333" offset="70%" />
-    </linearGradient>
-  </defs>
-  <rect width="${w}" height="${h}" fill="#333" />
-  <rect id="r" width="${w}" height="${h}" fill="url(#g)" />
-  <animate xlink:href="#r" attributeName="x" from="-${w}" to="${w}" dur="1s" repeatCount="indefinite"  />
-</svg>`
-
-  const toBase64 = (str: string) =>
-    typeof window === 'undefined' ? Buffer.from(str).toString('base64') : window.btoa(str)
-
   return (
-    <Card className="w-[350px] h-full flex flex-col	">
+    <Card className="w-[350px] h-screen flex flex-col	">
       <CardHeader>
         <CardTitle>{word.word_en}</CardTitle>
         <CardDescription>{word.transcription}</CardDescription>
