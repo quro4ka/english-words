d7408ff3efb217253b05bde8aa1aa29137ba5aba
quro4ka <quro4ka@gmail.com>
2023-08-04 17:08:16 +0300

    fead: add pwa

--- next.config.js
+++ next.config.js
@@ -1,5 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
+
+const withPWA = require('next-pwa')({
+  dest: 'public',
+  register: true,
+  skipWaiting: true,
+  disable: process.env.NODE_ENV === 'development',
+})
+
+const nextConfig = withPWA({
   images: {
     remotePatterns: [
       {
@@ -10,6 +18,6 @@ const nextConfig = {
       },
     ],
   },
-}
+})
 
 module.exports = nextConfig
